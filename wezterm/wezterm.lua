ed32ccda014bd1138bed1f156c99eff6396dbd72
PyGuy-Programming <pyguysprogramming2012@gmail.com>
2026-07-17 20:31:59 +0200

    added more configs :D

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+	default_cursor_style = "BlinkingBar",
+	color_scheme = "Catppuccin Macchiato",
+	window_background_opacity = 0.95,
+}
+
+return config
